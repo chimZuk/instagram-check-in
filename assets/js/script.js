199897d1cd3f8375c61b9d829b6867085885ebe0
chimZuk <dzmitry.kuzmitch@gmail.com>
2020-03-24 22:20:07 -0400

    Added cool input style

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,13 @@ function toggle_loading(state) {
 function on_container_scroll() {
     var container_boundaries = document.getElementById("container").getBoundingClientRect();
     var container_height = container_boundaries.bottom - container_boundaries.top;
-    var card_boundaries_percentage_string = "";
     var cards = document.getElementsByClassName("card");
 
     for (var i = 0; i < cards.length; i++) {
         var temp_boundaries = cards[i].getBoundingClientRect();
         var diff = container_boundaries.top - temp_boundaries.top;
         var diff_percentage = diff / container_height;
-        var percentage = (diff_percentage < -0.30 || diff_percentage > 0.30) ? (0.3 / diff_percentage) : 1;
-        card_boundaries_percentage_string += percentage + "\n";
+        var percentage = (diff_percentage < -0.20 || diff_percentage > 0.20) ? (0.2 / diff_percentage) : 1;
         cards[i].style.opacity = Math.abs(percentage);
     }
 }
@@ -26,6 +24,14 @@ function home_view() {
         <div class="card">
             <h1>Hey Bud!</h1>
             <p class="emoji">😉</p>
+        </div>
+        <div class="card">
+            <h1>I've made up 10k followers for my profile.</h1>
+            <p class="emoji">👨‍💻</p>
+        </div>
+        <div class="card">
+            <h1>So now...</h1>
+            <p class="emoji">👐</p>
         </div>
         <div class="card">
             <h1>What you see here...</h1>
@@ -46,6 +52,23 @@ function home_view() {
         <div class="card">
             <h1>Just leave me some ideas for a "Swipe Up" page!</h1>
             <p class="emoji">🤑</p>
+        </div>
+        <div class="card">
+            <h1>I will review submissions and make some of them live!</h1>
+            <p class="emoji">🎤</p>
+        </div>
+        <div class="card">
+            <h1>As well as feature all ideas in my Instagram Stories!</h1>
+            <p class="emoji">🤩</p>
+        </div>
+        <div class="card">
+            <h1>Bring it on! 🙃</h1>
+            <form>
+                <div class="input-block">
+                    <span class="at-element">@</span>
+                    <input type="text" name="nickname" placeholder="nickname"></input>
+                </div
+            </form>
         </div>
     `;
 }
