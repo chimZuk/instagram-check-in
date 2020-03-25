4d9e8cd5b1e0e9acb169b1c67b35ffc856101fdd
chimZuk <dzmitry.kuzmitch@gmail.com>
2020-03-24 21:49:47 -0400

    Added few style changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,49 @@ function toggle_loading(state) {
     state ? loading.style.display = "block" : loading.style.display = "none";
 }
 
+function on_container_scroll() {
+    var container_boundaries = document.getElementById("container").getBoundingClientRect();
+    var container_height = container_boundaries.bottom - container_boundaries.top;
+    var card_boundaries_percentage_string = "";
+    var cards = document.getElementsByClassName("card");
+
+    for (var i = 0; i < cards.length; i++) {
+        var temp_boundaries = cards[i].getBoundingClientRect();
+        var diff = container_boundaries.top - temp_boundaries.top;
+        var diff_percentage = diff / container_height;
+        var percentage = (diff_percentage < -0.30 || diff_percentage > 0.30) ? (0.3 / diff_percentage) : 1;
+        card_boundaries_percentage_string += percentage + "\n";
+        cards[i].style.opacity = Math.abs(percentage);
+    }
+
+    console.log(card_boundaries_percentage_string)
+}
+
 function home_view() {
     return `
         <div class="card">
-            <h1>Hey Bud! <span class="emoji">😉</span></h1>   
+            <h1>Hey Bud!</h1>
+            <p class="emoji">😉</p>
         </div>
         <div class="card">
-            <h1>What's you see here <span class="emoji">😏</span>...</h1>   
+            <h1>What you see here...</h1>
+            <p class="emoji">😏</p>
         </div>
         <div class="card">
-            <h1>And experiencing <span class="emoji">🤤</span>...</h1>   
+            <h1>And experiencing...</h1>
+            <p class="emoji">🤤</p>
         </div>
         <div class="card">
-            <h1>Is in fact <span class="emoji">🤔</span>..</h1>   
+            <h1>Is in fact...</h1>
+            <p class="emoji">🤔</p>
         </div>
         <div class="card">
-            <h1>Nothing that serious, LOL <span class="emoji">😅</span></h1>   
+            <h1>Nothing that serious.</h1>
+            <p class="emoji">😅</p>
         </div>
         <div class="card">
-            <h1>Just leave me some ideas for a "Swipe Up" page <span class="emoji">🤑</span></h1>   
+            <h1>Just leave me some ideas for a "Swipe Up" page!</h1>
+            <p class="emoji">🤑</p>
         </div>
     `;
 }
